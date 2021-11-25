423434b3667f1deeb3c156c30e54ef6b18860995
Felipe Muniz <35815542+fpmuniz@users.noreply.github.com>
2021-11-25 20:10:27 -0300

    Make index redirect to transactions + simpler month report

--- lib/budget_web/controllers/page_controller.ex
+++ lib/budget_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule BudgetWeb.PageController do
   use BudgetWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    redirect(conn, to: Routes.transaction_index_path(conn, :index))
   end
 end
