fcb9fe1a9e484aa1486e94ca9de59271bf1cc300
Felipe Muniz <35815542+fpmuniz@users.noreply.github.com>
2021-11-28 19:47:18 -0300

    Split reports / finances responsibilities

--- test/budget/reports_test.exs
+++ test/budget/reports_test.exs
@@ -3,69 +3,43 @@ defmodule Budget.ReportsTest do
   import Budget.Factory
   alias Budget.Reports
 
-  describe "get_monthly_report/2" do
-    setup do
-      sc1 = insert(:subcategory)
-      sc2 = insert(:subcategory)
-      _empty_subcategory = insert(:subcategory, category: sc2.category)
-      insert_list(5, :transaction, subcategory: sc1, value: 10)
-      insert_list(3, :transaction, subcategory: sc2, value: 20)
+  setup :today
 
-      %{
-        category1: sc1.category,
-        category2: sc2.category,
-        year: Date.utc_today().year,
-        month: Date.utc_today().month
-      }
+  describe "get_monthly_report/2" do
+    test "sums totals per category", %{year: year, month: month} do
+      category = insert(:category)
+      subcategory = insert(:subcategory, category: category)
+      insert_list(3, :transaction, subcategory: subcategory, value: 10)
+
+      assert %{category => Decimal.new("30")} == Reports.get_monthly_report(year, month)
     end
 
-    test "returns a hash of totals by category name", %{
-      year: year,
-      month: month,
-      category1: category1,
-      category2: category2
-    } do
-      expected = %{category1.name => Decimal.new("50"), category2.name => Decimal.new("60")}
-      assert expected == Reports.get_monthly_report(year, month)
+    test "groups multiple subcategories into their category", %{year: year, month: month} do
+      category = insert(:category)
+      subcat1 = insert(:subcategory, category: category)
+      subcat2 = insert(:subcategory, category: category)
+      insert_list(3, :transaction, subcategory: subcat1, value: 10)
+      insert_list(5, :transaction, subcategory: subcat2, value: 20)
+
+      assert %{category => Decimal.new("130")} == Reports.get_monthly_report(year, month)
     end
   end
 
-  describe "get_monthly_report_by_category/2" do
-    setup do
-      sc1 = insert(:subcategory)
-      sc2 = insert(:subcategory)
-      insert_list(5, :transaction, subcategory: sc1, value: 10)
-      insert_list(3, :transaction, subcategory: sc2, value: 20)
+  describe "get_yearly_report/1" do
+    setup :today
 
-      insert_list(2, :transaction,
-        subcategory: sc1,
-        value: 5,
-        due_by: Date.add(Date.utc_today(), -31)
-      )
+    test "returns a hash of totals in nested groupbys", %{year: year, month: month} do
+      category = insert(:category)
+      subcategory = insert(:subcategory, category: category)
+      insert_list(3, :transaction, subcategory: subcategory, value: 10)
 
-      %{
-        category1: sc1.category,
-        category2: sc2.category,
-        subcategory1: sc1,
-        subcategory2: sc2,
-        year: Date.utc_today().year
-      }
+      assert %{category => %{subcategory => %{month => Decimal.new("30")}}} ==
+               Reports.get_yearly_report(year)
     end
+  end
 
-    test "returns a hash of totals by category name", %{
-      year: year,
-      category1: cat1,
-      category2: cat2,
-      subcategory1: sc1,
-      subcategory2: sc2
-    } do
-      expected = [
-        {cat1.name, sc1.name, 10, Decimal.new(10)},
-        {cat1.name, sc1.name, 11, Decimal.new(50)},
-        {cat2.name, sc2.name, 11, Decimal.new(60)}
-      ]
-
-      assert expected == Reports.get_yearly_report(year)
-    end
+  defp today(context) do
+    %{month: month, year: year} = Date.utc_today()
+    Map.merge(context, %{month: month, year: year})
   end
 end
