3de214e2b5923f60a249c0fea114051e9d97ed06
Felipe Muniz <35815542+fpmuniz@users.noreply.github.com>
2021-11-28 21:02:59 -0300

    Decouple Reports module from Finances module
    This allows Reports to be tested in a coplete separate way compared to
    Finances, making it into an actual Domain / Context.

--- test/budget/reports_test.exs
+++ test/budget/reports_test.exs
@@ -3,43 +3,37 @@ defmodule Budget.ReportsTest do
   import Budget.Factory
   alias Budget.Reports
 
-  setup :today
+  describe "simple_report/1" do
+    test "sums totals per category" do
+      category = build(:category)
+      subcategory = build(:subcategory, category: category)
+      transactions = build_list(3, :transaction, subcategory: subcategory, value: 10)
 
-  describe "get_monthly_report/2" do
-    test "sums totals per category", %{year: year, month: month} do
-      category = insert(:category)
-      subcategory = insert(:subcategory, category: category)
-      insert_list(3, :transaction, subcategory: subcategory, value: 10)
-
-      assert %{category => Decimal.new("30")} == Reports.get_monthly_report(year, month)
+      assert %{category => Decimal.new("30")} == Reports.simple_report(transactions)
     end
 
-    test "groups multiple subcategories into their category", %{year: year, month: month} do
-      category = insert(:category)
-      subcat1 = insert(:subcategory, category: category)
-      subcat2 = insert(:subcategory, category: category)
-      insert_list(3, :transaction, subcategory: subcat1, value: 10)
-      insert_list(5, :transaction, subcategory: subcat2, value: 20)
+    test "groups multiple subcategories into their category" do
+      category = build(:category)
+      subcat1 = build(:subcategory, category: category)
+      subcat2 = build(:subcategory, category: category)
 
-      assert %{category => Decimal.new("130")} == Reports.get_monthly_report(year, month)
+      transactions =
+        build_list(3, :transaction, subcategory: subcat1, value: 10) ++
+          build_list(5, :transaction, subcategory: subcat2, value: 20)
+
+      assert %{category => Decimal.new("130")} == Reports.simple_report(transactions)
     end
   end
 
-  describe "get_yearly_report/1" do
-    setup :today
-
-    test "returns a hash of totals in nested groupbys", %{year: year, month: month} do
+  describe "detailed_report/1" do
+    test "returns a hash of totals in nested groupbys" do
       category = insert(:category)
       subcategory = insert(:subcategory, category: category)
-      insert_list(3, :transaction, subcategory: subcategory, value: 10)
+      transactions = insert_list(3, :transaction, subcategory: subcategory, value: 10)
+      month = Date.utc_today().month
 
       assert %{category => %{subcategory => %{month => Decimal.new("30")}}} ==
-               Reports.get_yearly_report(year)
+               Reports.detailed_report(transactions)
     end
-  end
-
-  defp today(context) do
-    %{month: month, year: year} = Date.utc_today()
-    Map.merge(context, %{month: month, year: year})
   end
 end
